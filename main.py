1e30d654a5e92423caf22ef53429fc97be6b10a1
Himanshu Singh <87076645+HimanshuSingh-all@users.noreply.github.com>
2024-01-31 04:15:03 +0530

    Decided to add more helper functions to help and save memory and possible computations during the runtime

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class indexDict(TypedDict):
 """
 
 
-def getword(str_inp:str)->dict:
+def getwords(str_inp:str)->dict:
 
     indexWords= dict()
     unique_words = 0
@@ -40,7 +40,28 @@ def nextword_freq(str_inp:str)->dict:
 
     return next_word_table
 
+def get_word_list(indexWords:dict):
+    """
+    Creates a List of Words to their corresponding indexes For ranking purposes
+    """
+    words = [0] * len(indexWords)
+
+    for word, index in indexWords:
+        words[index] = word
+
+    return words
+## Showing the possible coupling for a conversion to class abstraction in the future
+
+#indexWords_ = getwords()
+#WORD_LIST = get_word_list(indexWords_)
+
+
+def get_next_word_votes(word_table:np.ndarray)->np.ndarray:
+"""
+Get back the sorted of next words corresponding to the word in the given row index
+"""
+
 
 def get_next_word(cur_word:str , word_list: list ,word_table)->str:
-    """
-    """
+"""
+"""
