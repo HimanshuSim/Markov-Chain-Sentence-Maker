Added tests to check a basic functions, corrected some basic errors, next step will be to possibly apply the voting probability calculation and rank of the next word

project/main.py
@@ -14,9 +14,9 @@ def getwords(str_inp:str)->dict:
 
     str_inp=str_inp.rstrip()
     for line in str_inp.split('\n'):
-        for word in line:
-            if word not in dict:
-                dict[word]= unique_words
+        for word in line.split():
+            if word not in indexWords:
+                indexWords[word]= unique_words
                 unique_words+=1
     return indexWords
 
@@ -25,27 +25,27 @@ def nextword_freq(str_inp:str)->dict:
     """
     str_inp
     """
-    indexWords = getword(str_inp)
+    indexWords = getwords(str_inp)
     next_word_table = np.zeros( ( len(indexWords), len(indexWords) ) )
     str_inp=str_inp.rstrip()
     for line in str_inp.split('\n'):
-        for i,word in enumerate(line[:-1]):
+        line_words = line.split()
+        for i,word in enumerate(line_words[:-1]):
             # Get the next word
-            next_word = line[i+1]
+            next_word = line_words[i+1]
             #Get the respective index of word and the next word in the lookup table
             word_ind = indexWords[word]
             next_word_ind = indexWords[next_word]
             # Increment the value of the next word occurence corresponding to the current word
             next_word_table[word_ind, next_word_ind]+=1
 
-    return next_word_table
+    return next_word_table, indexWords
 
-def get_word_list(indexWords:dict):
+def get_word_list(indexWords:dict)->list:
     """
     Creates a List of Words to their corresponding indexes For ranking purposes
     """
     words = [0] * len(indexWords)
-
     for word, index in indexWords:
         words[index] = word
 
@@ -57,12 +57,22 @@ def get_word_list(indexWords:dict):
 
 
 def get_next_word_votes(word_table:np.ndarray)->np.ndarray:
-"""
-Get back the sorted of next words corresponding to the word in the given row index
-"""
+    """
+    Get back the sorted of next words corresponding to the word in the given row index
+    """
     pass
 
 def get_next_word(cur_word:str , word_list: list ,word_table)->str:
-"""
-"""
+    """
+    """
     pass
+
+
+if __name__ == '__main__':
+    string = 'hello world'#'the quick brown fox jumped over the lazy dog'
+    freqtable, indexes = nextword_freq(string)
+    print(" Freq Table: ")
+    print( freqtable )
+
+    print(" indexes")
+    print( indexes )